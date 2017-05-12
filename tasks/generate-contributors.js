c0337c5e057fc861006c15937c58746e50b46669
Marlon Bernardes <marlonjb.cb@gmail.com>
2017-05-12 12:52:34 +0200

    Fixed line breaks in contributors section

--- tasks/generate-contributors.js
+++ tasks/generate-contributors.js
@@ -61,7 +61,7 @@ function range(size) {
 
 function replaceContributorsBlock(filePath, content) {
   const contents = fs.readFileSync(filePath, 'utf8');
-  const newContents = contents.replace(/(?:(<!-- contributors:start -->))([^]+?)(?:(<!-- contributors:end -->))/, '$1' + content + '$3');
+  const newContents = contents.replace(/(?:(<!-- contributors:start -->))([^]+?)(?:(<!-- contributors:end -->))/, '$1\n' + content + '\n$3');
 
   fs.writeFileSync(filePath, newContents);
 }
